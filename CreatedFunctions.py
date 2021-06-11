3f7c044dfaa8b858bb21b8782ea0706e37ea8845
ismaeel <ismaeelsahib@gmail.com>
2021-06-11 20:37:52 +0500

    optimized code

--- CreatedFunctions.py
+++ CreatedFunctions.py
@@ -47,6 +47,8 @@ def lambda_handler(event, context):
     "message":""
     }
     regno = event.get('regno', None)
+    regno=str(regno.upper())
+    print(regno)
     firstname = event.get('firstname', None)
     lastname = event.get('lastname', None)
     section = event.get('section', None)
@@ -58,29 +60,25 @@ def lambda_handler(event, context):
         errors.append( 'errormessage : Invalid registration number ')
         errors.append( 'errormessage : Registration Number is not Entered ')
 
-    else:
-
-        if(check_if_item_exist(str(regno.upper())) ):
-            resp["success"]=False
-            resp["StatusCode"]=400
-            resp["message"]='Duplication of data may occur try with another registration number '
-            errors.append('errormessage : Data is already uploaded for ' + regno)
+    elif(check_if_item_exist(str(regno.upper())) or check_if_item_exist(str(regno.lower())) ):
+        # if (check_if_item_exist(str(regno.lower())) )
+        resp["success"]=False
+        resp["StatusCode"]=400
+        resp["message"]='Duplication of data may occur try with another registration number '
+        errors.append('errormessage : Data is already uploaded for ' + regno)
             
 
     
-        else:
-            if(validatesInput(firstname,lastname,section,errors,resp)):
-                response = table.put_item(Item=event)
-                resp["success"]=True
-                resp["StatusCode"]=200
-                resp["message"]='Date stored Successfully ' 
+    elif(validatesInput(firstname,lastname,section,errors,resp)):
+        response = table.put_item(Item=event)
+        resp["success"]=True
+        resp["StatusCode"]=200
+        resp["message"]='Date stored Successfully ' 
 
-                
+    else:
+        resp
     # if not errors:
     #     return resp
     # else:    
     resp.setdefault("errors", []).append(errors)            
     return resp
-
-
-
